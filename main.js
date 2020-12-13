8ede3bdcf7a44f8e1c098aa54199ba4daf51e360
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-12-13 11:40:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+img = "";
+status = "";
+objects=[];
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+function setup()
+{
+    canvas = createCanvas(640, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+
+}
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+    objectDetector.detect(img, gotResults);
+    document.getElementById('status').innerHTML = "Status: Detecting objects";
+}
+function gotResults(error, results)
+{
+   if(error)
+   {
+       console.log(error);
+   }else{
+       console.log(results);
+       objects = results;
+   }
+}
+function draw()
+
+{
+    image(img, 0, 0, 640, 400);
+    textSize(18);
+    if(status != "")
+    {
+      for(i=0; i <= objects.length; i++)
+       {
+        document.getElementById("status").innerHTML = "Status : Objects Detetcted";
+        fill('red');
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x +15, objects[i].y + 15 );
+        noFill();
+        stroke('red');
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height );
+       }
+    }
+        
+}
